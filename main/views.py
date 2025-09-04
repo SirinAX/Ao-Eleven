0571360dbd4f43fbee53223bd4aadbc38736f93c
unknown <hakimandi976@gmail.com>
2025-09-04 21:15:27 +0700

    Fix URL prefix for PWS

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from .models import Product
 
 def home(request):
     confg = {
-        'app_name': 'Ao Eleven',  # nama aplikasi kamu
+        'app_name': 'Ao Eleven',  
         'student_name': 'Andi Hakim Himawan',
         'class_name': 'PBP D',
         'featured': Product.objects.filter(is_featured=True)[:4],
