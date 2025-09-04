dd76ad005c0190fb472a5e94d54775884551f758
unknown <hakimandi976@gmail.com>
2025-09-04 20:49:47 +0700

    Early Testing

---
+++ main/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('jersey', 'Jersey'),
+        ('sepatu', 'Sepatu'),
+        ('bola', 'Bola'),
+        ('aksesoris', 'Aksesoris'),
+    ]
+
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    is_featured = models.BooleanField(default=False)
+
+    # Opsional fitur tambahan
+    stock = models.PositiveIntegerField(default=0)
+    brand = models.CharField(max_length=50, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Product
+
+
+def home(request):
+    confg = {
+        'app_name': 'Ao Eleven',  # nama aplikasi kamu
+        'student_name': 'Andi Hakim Himawan',
+        'class_name': 'PBP D',
+        'featured': Product.objects.filter(is_featured=True)[:4],
+    }
+    return render(request, 'main/home.html', confg)
