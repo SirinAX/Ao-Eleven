dc547128ca613e8260a460221ad360cb71fd7814
unknown <hakimandi976@gmail.com>
2025-09-06 11:48:04 +0700

    fixing

--- configure/urls.py
+++ configure/urls.py
@@ -9,7 +9,7 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    # path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
 
 # untuk development: media
