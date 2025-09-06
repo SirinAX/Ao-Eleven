dee1def2fc75f4a1508e7dd30ab5470ddc92b219
unknown <hakimandi976@gmail.com>
2025-09-06 09:39:07 +0700

    Testing server

--- configure/settings.py
+++ configure/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for configure project (revisi PWS + lokal tanpa FORCE_SCRIPT_NAME).
+Django settings for configure project Ao - Eleven 
 """
 
 from pathlib import Path
@@ -19,7 +19,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://andi-hakim42-aoeleven.pbp.cs.ui.ac.id'
 ]
 # Tentukan apakah environment production atau development
-# PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
+PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Application definition
 INSTALLED_APPS = [
@@ -63,31 +63,30 @@ WSGI_APPLICATION = 'configure.wsgi.application'
 
 from decouple import config
 
-# # Database configuration
-# if PRODUCTION:
-#     # Production: gunakan PostgreSQL
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv('DB_NAME'),
-#             'USER': os.getenv('DB_USER'),
-#             'PASSWORD': os.getenv('DB_PASSWORD'),
-#             'HOST': os.getenv('DB_HOST'),
-#             'PORT': os.getenv('DB_PORT'),
-#             'OPTIONS': {
-#                 'options': f"-c search_path={os.getenv('SCHEMA', 'PUBLIC')}"
-#             }
-#         }
-#     }
-# else:
-#     # Development: gunakan SQLite
-DATABASES = {
+# Database configuration
+if PRODUCTION:
+    # Production: gunakan PostgreSQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+            'OPTIONS': {
+                'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
+            }
+        }
+    }
+else:
+    # Development: gunakan SQLite
+ DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -105,11 +104,11 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'  
-STATIC_ROOT = BASE_DIR / 'static'  # collectstatic akan menaruh file di sini
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Media files
 MEDIA_URL = '/media/'  
-MEDIA_ROOT = BASE_DIR / 'media'  # pastikan folder media sama di lokal & PWS
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
