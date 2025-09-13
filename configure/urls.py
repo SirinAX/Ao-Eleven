b72b23568d137b5a0840a7cc3fc28ac06317814c
unknown <hakimandi976@gmail.com>
2025-09-13 09:47:38 +0700

    Commit Awal tugas 3 ( 4 fungsi views + Routing URL)

--- configure/urls.py
+++ configure/urls.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import home
+from main import views
 from django.conf import settings
 from django.conf.urls.static import static
-from main import views
-
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    # path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+
+    # Product views
+    path('products/', views.product_list, name='product_list'),
+    path('products/add/', views.product_add, name='product_add'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+
+    # Export JSON/XML
+    path('products/json/', views.products_json, name='products_json'),
+    path('products/xml/', views.products_xml, name='products_xml'),
+    path('products/json/<int:pk>/', views.product_json, name='product_json'),
+    path('products/xml/<int:pk>/', views.product_xml, name='product_xml'),
 ]
 
 # untuk development: media
--- main/views.py
+++ main/views.py
@@ -1,16 +1,57 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from django.core import serializers
 from .models import Product
+from .forms import ProductForm  # nanti buat forms.py
 
-
+# Halaman Home
 def home(request):
     confg = {
         'app_name': 'Ao Eleven',  
         'student_name': 'Andi Hakim Himawan',
         'class_name': 'PBP D',
-        # 'featured': Product.objects.filter(is_featured=True)[:4],
     }
     return render(request, 'main/home.html', confg)
 
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     return render(request, "main/product_detail.html", {"product": product})
+# 1. List view semua produk
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'main/product_list.html', {'products': products})
+
+# 2. Detail view produk berdasarkan ID
+def product_detail(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    return render(request, 'main/product_detail.html', {'product': product})
+
+# 3. Form untuk menambahkan produk baru
+def product_add(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')  # redirect ke list view
+    else:
+        form = ProductForm()
+    return render(request, 'main/product_add.html', {'form': form})
+
+# 4. Export semua produk ke JSON
+def products_json(request):
+    data = serializers.serialize('json', Product.objects.all())
+    return HttpResponse(data, content_type='application/json')
+
+# 5. Export semua produk ke XML
+def products_xml(request):
+    data = serializers.serialize('xml', Product.objects.all())
+    return HttpResponse(data, content_type='application/xml')
+
+# 6. Export detail produk ke JSON
+def product_json(request, pk):
+    product = Product.objects.filter(id=pk)
+    data = serializers.serialize('json', product)
+    return HttpResponse(data, content_type='application/json')
+
+# 7. Export detail produk ke XML
+def product_xml(request, pk):
+    product = Product.objects.filter(id=pk)
+    data = serializers.serialize('xml', product)
+    return HttpResponse(data, content_type='application/xml')
