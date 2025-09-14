b9d80e3e2c4c0b70ff0db2d2113c2a5aa04a4637
unknown <hakimandi976@gmail.com>
2025-09-14 10:14:52 +0700

    Fixing bugs (again)

--- configure/urls.py
+++ configure/urls.py
@@ -6,23 +6,18 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Home / daftar produk
     path('', views.home, name='home'),
 
     # Product views
-    path('products/add/', views.add_product, name='add_product'),
+    path('products/add/', views.product_add, name='product_add'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
 
-    # Export JSON/XML (all products)
+    # Export JSON/XML
     path('products/json/', views.products_json, name='products_json'),
     path('products/xml/', views.products_xml, name='products_xml'),
-
-    # Export JSON/XML (single product by ID)
     path('products/json/<int:pk>/', views.product_json, name='product_json'),
     path('products/xml/<int:pk>/', views.product_xml, name='product_xml'),
 ]
 
-# untuk serve media saat development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# untuk development: media
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
