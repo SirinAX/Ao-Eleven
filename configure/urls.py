b72d297af1a43fdee4c01270cf1a07de385be76c
unknown <hakimandi976@gmail.com>
2025-09-14 10:27:18 +0700

    fxing urls.py bug

--- configure/urls.py
+++ configure/urls.py
@@ -4,6 +4,8 @@ from main import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = "main"   
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
